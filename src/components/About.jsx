3d475a19c680259200c32f49feb63d9449acc43f
duynguyen-workspace <duynguyen.workspace@gmail.com>
2024-03-20 01:04:03 +0700

    Complete template for Hero, About, Experience, Tech, and WOrk section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { Tilt }from "react-tilt";
 
-const About = () => {
-  return (
-    <div>About</div>
-  )
+import { styles } from "../utils/styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motions";
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({ index, title, icon }) => {
+    return (
+        <Tilt className="xs:w-[250px] w-full">
+            <motion.div 
+                variants={fadeIn("right", "sprint", 0.5 * index, 0.75)}
+                className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+            >
+                <div 
+                    options={{
+                        max: 45,
+                        scale: 1,
+                        speed: 450
+                    }}
+                    className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+                >
+                    <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+                    <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+
+                </div>
+
+            </motion.div>
+        </Tilt>
+    )
 }
 
-export default About
+const About = () => {
+    return (
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>Introduction</p>
+                <h2 className={styles.sectionHeadText}>Overview.</h2>
+            </motion.div>
+
+            <motion.p 
+                variants={fadeIn("", "", 0.1, 1)}
+                className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+            >
+                As a final-year student majoring in Information Technology, I have more than 2 years of educational experience with different programming languages (especially Java & JavaScript), technologies and frameworks. I pursuit my career as a professional full-stack software developer, and eager to learn new technologies related to programming areas, with passion, diligence, and professionalism. Hope to work with you!
+            </motion.p>
+
+            <div className="mt-20 flex flex-wrap gap-10">
+                {services.map((service, index) => (
+                    <ServiceCard key={service.title} index={index} {...service} />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default SectionWrapper(About, "about");
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
 
-const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-export default Computers
+import CanvasLoader from "../CanvasLoader";
+
+const Computers = ({ isMobile }) => {
+    const computer = useGLTF('./desktop_pc/scene.gltf');
+
+    return (
+        <mesh>
+            <hemisphereLight intensity={2} groundColor="black" />
+            <pointLight intensity={1}/> 
+            <spotLight
+                position={[-20, 50, 10]}
+                angle={10}
+                penumbra={1}
+                intensity={Math.PI}
+                castShadow
+                shadow-mapSize={1024}
+            />
+            <primitive 
+                object={computer.scene} 
+                scale={isMobile ? 0.7 : 0.85}
+                position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+                rotation={[-0.01, -0.2, -0.1]}
+            />
+        </mesh>
+    );
+};
+
+const ComputersCanvas = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Add a listener for changes to the screen size
+        const mediaQuery = window.matchMedia('(max-width: 700px)')
+
+        // Set initial value to state variable
+        setIsMobile(mediaQuery.matches)
+
+        //* Callback function: handle changes to media query
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches)
+        }
+
+        // Add the callback function as a listener
+        mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+        // Remove the listener when the component is unmounted
+        return () => {
+            mediaQuery.removeEventListener('change', handleMediaQueryChange)
+        }
+    }, [])
+
+    return (
+        <Canvas
+            frameLoop="demand"
+            shadows
+            camera={{ position: [20, 3, 5], fov: 25 }}
+            gl={{ preserveDrawingBuffer: true }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <Computers isMobile={isMobile}/>
+            </Suspense>
+
+            <Preload all />
+        </Canvas>
+    );
+};
+
+export default ComputersCanvas;
